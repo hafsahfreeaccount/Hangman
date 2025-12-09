a9f0f2f83c8459d3ab3fb1ff94b375c70adfd92d
rushda-101 <rushda.afaf@gmail.com>
2025-12-09 23:14:12 +0000

    Start of the homepage + landing screen

---
+++ homescreen.js
@@ -0,0 +1,8 @@
+// HOME SCREEN PLAY BUTTON
+const playButton = document.getElementById("playBtn");
+
+if (playButton) {
+    playButton.addEventListener("click", function () {
+        window.location.href = "landing.html"; 
+    });
+}
---
+++ landing.js
@@ -0,0 +1,4 @@
+// LANDING SCREEN DELAY -> MOVE TO DIFFICULTY SCREEN
+setTimeout(() => {
+    window.location.href = "difficulty.html";
+}, 2000); // 2-second loading screen
