79dc6034855a129e7dfafcd4b6a89a2c13ead239
rushda-101 <rushda.afaf@gmail.com>
2025-12-16 19:44:13 +0000

    Added finished overlay for pause button

--- JavaScript/gameplay.js
+++ JavaScript/gameplay.js
@@ -5,3 +5,43 @@ if (instructionsBtn) {
         window.location.href = "instructions.html";
     });
 }
+
+
+const pauseBtn = document.getElementById("pauseBtn");
+const pauseOverlay = document.getElementById("pauseOverlay");
+
+const closePauseBtn = document.getElementById("closePauseBtn");
+const resumeBtn = document.getElementById("resumeBtn");
+const restartBtn = document.getElementById("restartBtn");
+
+const homeBtn = document.getElementById("homeBtn");
+const pauseInstructionsBtn = document.getElementById("pauseInstructionsBtn");
+
+/* Open pause */
+pauseBtn.addEventListener("click", () => {
+    pauseOverlay.style.display = "flex";
+});
+
+/* Close / Resume */
+closePauseBtn.addEventListener("click", () => {
+    pauseOverlay.style.display = "none";
+});
+
+resumeBtn.addEventListener("click", () => {
+    pauseOverlay.style.display = "none";
+});
+
+/* Restart */
+restartBtn.addEventListener("click", () => {
+    location.reload();
+});
+
+/* Go Home */
+homeBtn.addEventListener("click", () => {
+    window.location.href = "homescreen.html";
+});
+
+/* Instructions */
+pauseInstructionsBtn.addEventListener("click", () => {
+    window.location.href = "instructions.html";
+});
