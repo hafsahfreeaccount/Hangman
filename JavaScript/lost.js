1377ebb1e3c5543da82c5660b0558e254a0a1d7f
khadijahi <109248890+tranquilts@users.noreply.github.com>
2025-12-17 12:49:30 +0000

    Correct player storage logic and event handling
    Fix issues with player storage and button event listeners.

--- JavaScript/lost.js
+++ JavaScript/lost.js
@@ -1,7 +1,36 @@
-const instructionsBtn = document.getElementById("instructionsBtn");
+let currentPlayer = localStotage.getItem("currentPlayer");
 
-if (instructionsBtn) {
-    instructionsBtn.addEventListener("click", () => {
-        window.location.href = "instructions.html";
+const nextBtn = document.getElementById("nextBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+// Save current player during gameplay
+let currentPlayer = localStorage.getItem("currentPlayer");
+
+if (!currentPlayer) {
+    currentPLayer =  "1";
+    locaStorage.setItem("currentPlayer", "!");
+
+    
+if  (nextBtn) {
+    nextBtn.addEventListener("click", ()  => {
+        if currentPlayer === "1") {
+
+        localStorage.setItem("currentPlayer, "2");
+            window.location.href = "gameplay.html";
+        } else {
+
+        window.location.href = "scoreboard.html";
+        }
     });
 }
+    
+const scoreDisplay = document.getElementById ("scoreValue");
+const currentPlayer = localStorage.getItem("currentPlayer");
+
+    if (currentPlayer == "1") {
+scoreDisplay.textContent = localStorage.getItem ("player1Score"); 
+    } else {
+        scoreDisplay.textContent = localStoragge.getItem("player2Score");
+    }
+
+
